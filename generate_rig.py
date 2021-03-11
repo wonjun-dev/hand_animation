4077fbd41e67136459e8d7a96f1cfee9c3e10e93
wonjun-dev <wonjun.dev@gmail.com>
2021-03-11 16:35:10 +0900

    bone connection

--- generate_rig.py
+++ generate_rig.py
@@ -7,10 +7,34 @@ import json
 import bpy
 import numpy as np
 
-os.chdir("/home/wonjun/Blender/blender-2.91.2-linux64/projects/dancing_mesh")
+os.chdir("/home/wonjun/Blender/blender-2.91.2-linux64/projects/hand_animation")
 
 json_path = "source/output/json"
 json_name = "coords.json"
+
+
+BONE_CONNECTION = [
+    [0, 1],
+    [0, 5],
+    [0, 9],
+    [0, 13],
+    [0, 17],
+    [1, 2],
+    [2, 3],
+    [3, 4],
+    [5, 6],
+    [6, 7],
+    [7, 8],
+    [9, 10],
+    [10, 11],
+    [11, 12],
+    [13, 14],
+    [14, 15],
+    [15, 16],
+    [17, 18],
+    [18, 19],
+    [19, 20],
+]
 
 
 def read_json(path):
